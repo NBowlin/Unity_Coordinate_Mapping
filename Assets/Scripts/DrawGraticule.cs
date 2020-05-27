f6d91fe3175408a565f042b30ee3400e7d4ef79b
Nick Bowlin <ngb5601@gmail.com>
2020-05-27 14:10:59 -0400

    Adding initial longitude drawing

---
+++ Assets/Scripts/DrawGraticule.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGraticule : MonoBehaviour
+{
+    [SerializeField] private LineRenderer line;
+
+    public int segments;
+    public float angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line.positionCount = segments;
+        line.startWidth = 0.005f;
+        line.endWidth = 0.005f;
+        line.useWorldSpace = false;
+        line.loop = true;
+        line.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+        DrawLongitudeLines();
+        //DrawLatitudeLines();
+    }
+
+    void DrawLongitudeLines() {
+            var longitude = Quaternion.AngleAxis(angle, Vector3.up) * Vector3.forward;
+
+            int segmentIncrement = 360 / segments;
+            int currentSegment = 0;
+            for (int j = 0; j < 360; j += segmentIncrement) {
+                var point = Quaternion.AngleAxis((float)j, longitude) * Vector3.up;
+                point *= 0.502f;
+                line.SetPosition(currentSegment, point);
+                currentSegment += 1;
+            }
+    }
+
+    void DrawLatitudeLines() {
+        var longitude = Vector3.up; //Quaternion.AngleAxis(angle, Vector3.up) * Vector3.forward;
+
+        int segmentIncrement = 360 / segments;
+        int currentSegment = 0;
+        for (int j = 0; j < 360; j += segmentIncrement) {
+            var point = Quaternion.AngleAxis((float)j, longitude) * Vector3.right;
+            point *= 0.502f;
+            line.SetPosition(currentSegment, point);
+            currentSegment += 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GenerateGrid.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateGrid : MonoBehaviour
+{
+    public GameObject graticulePrefab;
+
+    [SerializeField] private int segments;
+    [SerializeField] private int angleIncrement;
+
+    // Start is called before the first frame update
+    void Start() {
+        DrawLongitudeLines();
+    }
+
+    void DrawLongitudeLines() {
+        for (int i = 0; i < 360; i += angleIncrement) {
+
+            var graticuleGO = Instantiate(graticulePrefab, transform);
+            var graticule = graticuleGO.GetComponent<DrawGraticule>();
+            graticule.segments = segments;
+            graticule.angle = i;
+
+            /*var longitude = Quaternion.AngleAxis((float)i, Vector3.up) * Vector3.right;
+
+            int segmentIncrement = 360 / segments;
+            int currentSegment = 0;
+            for (int j = 0; j < 360; j += segmentIncrement) {
+                var point = Quaternion.AngleAxis((float)j, longitude) * Vector3.up;
+                point *= 0.5f;
+                //Debug.DrawRay(transform.localPosition, point, Color.red, 100.0f);
+                Debug.Log("Add point: " + point);
+                //line.SetPosition(currentSegment, point);
+                currentSegment += 1;
+            }*/
+            //var latitude = Quaternion.AngleAxis((float)i, Vector3.up) * Vector3.right;
+
+        }
+    }
+}
