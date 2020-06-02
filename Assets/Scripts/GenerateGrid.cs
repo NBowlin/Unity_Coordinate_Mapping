5bbc240bf90b75a40665005d029c1d8a710939c6
Nick Bowlin <ngb5601@gmail.com>
2020-06-02 14:04:27 -0400

    Adding meridians to custom graticule mesh

--- Assets/Scripts/GenerateGrid.cs
+++ Assets/Scripts/GenerateGrid.cs
@@ -70,7 +70,9 @@ public class GenerateGrid : MonoBehaviour {
     }
 
     void DrawUsingMeshTopologyLines() {
+        //TODO: Clean this up so there isn't so much duplication
         Mesh parallelsMesh = new Mesh();
+        Mesh meridiansMesh = new Mesh();
 
         var graticuleContainer = new GameObject("Graticule");
         graticuleContainer.transform.SetParent(transform, false);
@@ -83,46 +85,67 @@ public class GenerateGrid : MonoBehaviour {
         var pMeshRenderer = parallelsContainer.AddComponent<MeshRenderer>();
         pMeshRenderer.material = lineMat;
 
-
         var meridiansContainer = new GameObject("Meridians");
         meridiansContainer.transform.SetParent(graticuleContainer.transform, false);
-        meridiansContainer.AddComponent<MeshFilter>();
-        meridiansContainer.AddComponent<MeshRenderer>();
+        var mFilter = meridiansContainer.AddComponent<MeshFilter>();
+        mFilter.mesh = meridiansMesh;
+        var mMeshRenderer = meridiansContainer.AddComponent<MeshRenderer>();
+        mMeshRenderer.material = lineMat;
 
         int segmentIncrement = 360 / segments;
         var parallelsPoints = new List<List<Vector3>>();
+        var meridianPoints = new List<List<Vector3>>();
 
         for (int i = -90; i <= 90; i += degreesBetweenLines) {
             var currParallelPoints = new List<Vector3>();
+            var curMeridianPoints = new List<Vector3>();
             for (int j = 0; j < 360; j += segmentIncrement) {
 
-                var line = Quaternion.Euler(0.0f, j, i) * Vector3.right;
+                var latLine = Quaternion.Euler(0.0f, j, i) * Vector3.right;
+                var lngLine = Quaternion.Euler(0.0f, i, j) * Vector3.right;
 
                 //Need to reverse the ray direction because collisions don't work from the inside of a collider
                 //So take a point some distance along the line as origin, then reverse the direction
-                var ray = new Ray(transform.position, line * 200.0f);
-                ray.origin = ray.GetPoint(200.0f);
-                ray.direction = -ray.direction;
+                var latRay = new Ray(transform.position, latLine * 200.0f);
+                latRay.origin = latRay.GetPoint(200.0f);
+                latRay.direction = -latRay.direction;
 
-                RaycastHit hit;
+                RaycastHit latHit;
 
-                if (Physics.Raycast(ray, out hit)) {
-                    if (hit.collider.gameObject.tag == "Earth") {
-                        currParallelPoints.Add(hit.point);
+                if (Physics.Raycast(latRay, out latHit)) {
+                    if (latHit.collider.gameObject.tag == "Earth") {
+                        currParallelPoints.Add(latHit.point);
                     }
                 }
                 else {
                     Debug.Log("Raycast missed Earth");
-                    Debug.DrawRay(ray.origin, ray.direction * 200.0f, Color.yellow, 1000.0f);
+                    Debug.DrawRay(latRay.origin, latRay.direction * 200.0f, Color.yellow, 1000.0f);
+                }
+
+                var lngRay = new Ray(transform.position, lngLine * 200.0f);
+                lngRay.origin = lngRay.GetPoint(200.0f);
+                lngRay.direction = -lngRay.direction;
+
+                RaycastHit lngHit;
+
+                if (Physics.Raycast(lngRay, out lngHit)) {
+                    if (lngHit.collider.gameObject.tag == "Earth") {
+                        curMeridianPoints.Add(lngHit.point);
+                    }
+                }
+                else {
+                    Debug.Log("Raycast missed Earth");
+                    Debug.DrawRay(lngRay.origin, lngRay.direction * 200.0f, Color.yellow, 1000.0f);
                 }
             }
 
             if (currParallelPoints.Count > 1) { parallelsPoints.Add(currParallelPoints); }
+            if (curMeridianPoints.Count > 1) { meridianPoints.Add(curMeridianPoints); }
         }
 
-        var allPoints = parallelsPoints.SelectMany(i => i).ToArray<Vector3>();
+        var allParallelsPoints = parallelsPoints.SelectMany(i => i).ToArray<Vector3>();
 
-        var parallelsIndices = new int[(allPoints.Length * 2)];
+        var parallelsIndices = new int[(allParallelsPoints.Length * 2)];
         var currIndex = 0;
 
         for (int i = 0; i < parallelsPoints.Count; i++) {
@@ -138,8 +161,29 @@ public class GenerateGrid : MonoBehaviour {
             currIndex += 2;
         }
 
-        parallelsMesh.vertices = allPoints;
+        parallelsMesh.vertices = allParallelsPoints;
         parallelsMesh.SetIndices(parallelsIndices, MeshTopology.Lines, 0, true);
+
+        var allMerdianPoints = meridianPoints.SelectMany(i => i).ToArray<Vector3>();
+
+        var meridianIndices = new int[(allMerdianPoints.Length * 2)];
+        var currMeridianIndex = 0;
+
+        for (int i = 0; i < meridianPoints.Count; i++) {
+            var currPoints = meridianPoints[i];
+            for (int j = 1; j < currPoints.Count; j++) {
+                int adjustedIndex = currMeridianIndex - 2 * i;
+                meridianIndices[currMeridianIndex] = Mathf.CeilToInt(adjustedIndex / 2f) + i;
+                meridianIndices[currMeridianIndex + 1] = Mathf.CeilToInt((adjustedIndex + 1) / 2f) + i;
+                currMeridianIndex += 2;
+            }
+            meridianIndices[currMeridianIndex] = i * segments + currPoints.Count - 1;
+            meridianIndices[currMeridianIndex + 1] = i * segments;
+            currMeridianIndex += 2;
+        }
+
+        meridiansMesh.vertices = allMerdianPoints;
+        meridiansMesh.SetIndices(meridianIndices, MeshTopology.Lines, 0, true);
 
         //mesh.vertices = points;
         //mesh.SetIndices(new int[] { 0, 1, 1, 2, 3, 4 }, MeshTopology.Lines, 0, true);
