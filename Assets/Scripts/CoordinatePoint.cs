bcbd23944e40b93a135071de07d3264f67c59ebc
Nick Bowlin <ngb5601@gmail.com>
2020-06-05 10:35:20 -0400

    - Adjusting architecture for coordinate mapping to support multiple coordinate point types - Tweaked Earth size

---
+++ Assets/Scripts/CoordinatePoint.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable] public class CoordinatePoint
+{
+    public GameObject pointPrefab;
+    public Location location;
+
+    [SerializeField] private float placingAdjustment = 0f; //Used to adjust point inward / outward from surface
+
+    public virtual GameObject Plot(Transform planet, Transform container) {
+        return PlacePoint(planet, container);
+    }
+
+    private GameObject PlacePoint(Transform planet, Transform container) {
+        var point = Quaternion.Euler(0.0f, -location.longitude, location.latitude) * Vector3.right;
+
+        var hitInfo = PlanetUtility.LineFromOriginToSurface(planet, point);
+        if (hitInfo.HasValue) {
+            var go = UnityEngine.Object.Instantiate(pointPrefab, hitInfo.Value.point + point * placingAdjustment, Quaternion.LookRotation(-point), container);
+            go.name = location.name;
+            return go;
+        }
+
+        return null;
+    }
+}
+
+[Serializable] public class CoordinatePoint_Magnitude: CoordinatePoint {
+    public float magnitude;
+
+    public override GameObject Plot(Transform planet, Transform container) {
+        var go = base.Plot(planet, container);
+        if(go != null) { go.transform.localScale = new Vector3(go.transform.localScale.x, go.transform.localScale.y, go.transform.localScale.z * magnitude); }
+
+        return go;
+    }
+}
--- Assets/Scripts/CoordinatesMapper.cs
+++ Assets/Scripts/CoordinatesMapper.cs
@@ -4,28 +4,15 @@ using UnityEngine;
 
 public class CoordinatesMapper : MonoBehaviour
 {
-    [SerializeField] private GameObject pointPrefab;
-    [SerializeField] private List<Location> locations;
-
-    [SerializeField] private float placingAdjustment = 0f; //Used to adjust point inward / outward from surface
+    [SerializeField] private List<CoordinatePoint> locations;
 
     private void Start() {
 
         var locationsContainer = new GameObject("Locations");
         locationsContainer.transform.SetParent(transform, false);
 
-        foreach (Location loc in locations) {
-            MapLocation(loc, locationsContainer.transform);
-        }
-    }
-
-    private void MapLocation(Location loc, Transform parentContainer) {
-        var point = Quaternion.Euler(0.0f, -loc.longitude, loc.latitude) * Vector3.right;
-
-        var hitInfo = PlanetUtility.LineFromOriginToSurface(transform, point);
-        if(hitInfo.HasValue) {
-            var go = Instantiate(pointPrefab, hitInfo.Value.point + point * placingAdjustment, Quaternion.LookRotation(-point), parentContainer);
-            go.name = loc.name;
+        foreach (CoordinatePoint point in locations) {
+            point.Plot(transform, locationsContainer.transform);
         }
     }
 }
