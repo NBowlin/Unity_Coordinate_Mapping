33f18e97a32c3b0376ed04483f9a4e40abd83160
Nick Bowlin <ngb5601@gmail.com>
2020-06-26 16:06:31 -0400

    Reworking the DataLoader classes so they only deserialize the json and invoke an event with the CoordinatePoints rather than deserializing and then instantiating points

--- Packages/Coordinate-Mapper/Runtime/Scripts/Coordinates/CoordinatesMapper.cs
+++ Packages/Coordinate-Mapper/Runtime/Scripts/Coordinates/CoordinatesMapper.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 namespace CoordinateMapper.Coordinates {
     public class CoordinatesMapper : MonoBehaviour {
-        [SerializeField] private List<CoordinatePoint_Basic> locations;
-        [SerializeField] private List<CoordinatePoint_Magnitude> magLocations;
+        //[SerializeField] private List<CoordinatePoint> locations;
+        [SerializeField] private GameObject pointPrefab;
 
         private void Start() {
-            var locationsContainer = new GameObject("Locations");
+            /*var locationsContainer = new GameObject("Locations");
             locationsContainer.transform.SetParent(transform, false);
 
             foreach (CoordinatePoint point in locations) {
@@ -20,8 +20,19 @@ namespace CoordinateMapper.Coordinates {
 
             foreach (CoordinatePoint_Magnitude point in magLocations) {
                 point.Plot(transform, magLocationsContainer.transform);
-            }
+            }*/
+        }
 
+        public void MapPoints(IEnumerable<CoordinatePoint> p) {
+            Debug.Log("Map points: " + p);
+
+            var container = new GameObject("Locs");
+            container.transform.SetParent(transform, false);
+
+            foreach (CoordinatePoint point in p) {
+                point.pointPrefab = pointPrefab; //TODO: Rework this
+                point.Plot(transform, container.transform);
+            }
         }
     }
 }
--- Packages/Coordinate-Mapper/Runtime/Scripts/Data/DataLoader.cs
+++ Packages/Coordinate-Mapper/Runtime/Scripts/Data/DataLoader.cs
@@ -1,36 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 using CoordinateMapper.Coordinates;
 
 namespace CoordinateMapper.Data {
+
+    [System.Serializable] public class JsonLoadedEvent : UnityEvent<IEnumerable<CoordinatePoint>> { }
+
     //TODO: Better way to do this without needing a subclass for each coordinate point type?
     public abstract class DataLoader : MonoBehaviour {
-        [SerializeField] private GameObject pointPrefab;
+        //[SerializeField] private GameObject pointPrefab;
         [SerializeField] protected TextAsset jsonFile;
-        [SerializeField] private string containerName;
+        //[SerializeField] private string containerName;
 
-        [SerializeField] private Heatmap heatmap;
+        [SerializeField] private JsonLoadedEvent loadComplete;
 
         // Start is called before the first frame update
         void Start() {
             var points = LoadJson();
+            if(loadComplete != null) { loadComplete.Invoke(points); }
 
             //TODO: Rework this - just for testing
-            if (heatmap != null) {
+            /*if (heatmap != null) {
                 heatmap.DoStuff(points);
             }
             else {
-                LoadData(points);
-            }
+                //LoadData(points);
+            }*/
         }
 
         //We have to return IEnumerable so we get Covariance and can return subclasses of CoordinatePoint
         //Explanation: https://stackoverflow.com/questions/4652858/why-cant-i-assign-a-listderived-to-a-listbase/4653199
         protected abstract IEnumerable<CoordinatePoint> LoadJson();
 
-        private void LoadData(IEnumerable<CoordinatePoint> points) {
+        /*private void LoadData(IEnumerable<CoordinatePoint> points) {
             var container = new GameObject(containerName);
             container.transform.SetParent(transform, false);
 
@@ -38,6 +43,6 @@ namespace CoordinateMapper.Data {
                 point.pointPrefab = pointPrefab; //TODO: Rework this
                 point.Plot(transform, container.transform);
             }
-        }
+        }*/
     }
 }
