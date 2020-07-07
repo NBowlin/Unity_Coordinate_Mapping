fb6caf8569627370e80e7146f3efab6c4cde4b1f
Nick Bowlin <ngb5601@gmail.com>
2020-07-07 17:52:21 -0400

    Handling different JSON parsing options

--- Packages/Coordinate-Mapper/Runtime/Scripts/Data/JsonDataLoader.cs
+++ Packages/Coordinate-Mapper/Runtime/Scripts/Data/JsonDataLoader.cs
@@ -38,36 +38,92 @@ namespace CoordinateMapper.Data {
             var tokens = parsed.AllTokens();
 
             var allPoints = new List<T>();
-            var latProperties = new List<JProperty>();
-            var lngProperties = new List<JProperty>();
+            var latProperties = tokens.Where(t => t.Type == JTokenType.Property && ((JProperty)t).Name == latitudeKey).Select(p => (JProperty)p).ToList();
+            var lngProperties = tokens.Where(t => t.Type == JTokenType.Property && ((JProperty)t).Name == longitudeKey).Select(p => (JProperty)p).ToList();
 
             switch (parseStyle) {
                 case JsonParseStyle.LatAndLngKeys:
-                    latProperties = tokens.Where(t => t.Type == JTokenType.Property && ((JProperty)t).Name == latitudeKey).Select(p => (JProperty)p).ToList();
-                    lngProperties = tokens.Where(t => t.Type == JTokenType.Property && ((JProperty)t).Name == longitudeKey).Select(p => (JProperty)p).ToList();
-                    break;
+                    return ParseSeparateLatLngKeys(latProperties, lngProperties);
                 case JsonParseStyle.LatLngArrays:
-                    break;
+                    return ParseSeparateLatLngArrays(latProperties, lngProperties);
                 case JsonParseStyle.SingleLatLngArray:
-                    break;
+                    return ParseAlternatingLatLngArray(latProperties);
                 default:
-                    break;
+                    return new List<T>();
+            }
+        }
+
+        private static List<T> ParseSeparateLatLngKeys(List<JProperty> latProperties, List<JProperty> lngProperties) {
+
+            var latVals = latProperties.Select(v => v.Value).ToList();
+            var lngVals = lngProperties.Select(v => v.Value).ToList();
+
+            //TODO: Just return null if empty?
+            return CreatePointsFromLatLngs(latVals, lngVals) ?? new List<T>();
+        }
+
+        private static List<T> ParseSeparateLatLngArrays(List<JProperty> latArrayProperties, List<JProperty> lngArrayProperties) {
+            if (latArrayProperties.Count != lngArrayProperties.Count) {
+                Debug.Log("Different number of latitude and longitude arrays");
+                return null;
             }
 
-            if (latProperties.Count != lngProperties.Count) {
+            var allPoints = new List<T>();
+
+            for (int i = 0; i < latArrayProperties.Count; i++) {
+                var latArr = (JArray)latArrayProperties[i].Value;
+                var lngArr = (JArray)lngArrayProperties[i].Value;
+
+                var latList = latArr.Children().ToList();
+                var lngList = lngArr.Children().ToList();
+
+                var setPoints = CreatePointsFromLatLngs(latList, lngList);
+                if(setPoints?.Count > 0) { allPoints.AddRange(setPoints); }
+            }
+
+            return allPoints;
+        }
+
+        private static List<T> ParseAlternatingLatLngArray(List<JProperty> coordsArrayProperties) {
+            var allPoints = new List<T>();
+
+            for (int i = 0; i < coordsArrayProperties.Count; i++) {
+
+                var coordsArr = (JArray)coordsArrayProperties[i].Value;
+
+                if (coordsArr.Count % 2 != 0) {
+                    Debug.Log("Different number of latitude and longitude in alternating array -- Skipping set");
+                    continue;
+                }
+
+                var latVals = coordsArr.Children().Where((v, index) => index % 2 == 0).ToList();
+                var lngVals = coordsArr.Children().Where((v, index) => index % 2 == 1).ToList();
+
+                var setPoints = CreatePointsFromLatLngs(latVals, lngVals);
+                if (setPoints?.Count > 0) { allPoints.AddRange(setPoints); }
+            }
+
+            return allPoints;
+        }
+
+        private static List<T> CreatePointsFromLatLngs(List<JToken> lats, List<JToken> lngs) {
+
+            var allPoints = new List<T>();
+
+            if (lats.Count != lngs.Count) {
                 Debug.Log("Different amount of latitudes and longitudes?");
                 return null;
             }
 
-            for (int i = 0; i < latProperties.Count; i++) {
-                var latVal = latProperties[i].Value;
-                var lngVal = lngProperties[i].Value;
+            for (int j = 0; j < lats.Count; j++) {
+                var latVal = lats[j];
+                var lngVal = lngs[j];
 
                 float lat = 0f;
                 float lng = 0f;
 
                 //TODO: Should we assume that lat and lng use the same type?
-                switch(latVal.Type) {
+                switch (latVal.Type) {
                     case JTokenType.Float:
                     case JTokenType.Integer:
                         lat = (float)latVal;
