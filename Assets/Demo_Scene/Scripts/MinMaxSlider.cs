ea90b16bfb6ea14c95e98140b471279589713096
Nick Bowlin <ngb5601@gmail.com>
2020-07-22 17:46:11 -0400

    WIP: Implementing a 2 handle slider with min and max value

---
+++ Assets/Demo_Scene/Scripts/MinMaxSlider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MinMaxSlider : MonoBehaviour, IDragHandler
+{
+    private float minX = 0f;
+    private float maxX = 0f;
+
+    RectTransform rt;
+
+    private void Start() {
+        rt = (RectTransform)transform;
+        minX = rt.rect.width / 2;
+        maxX = ((RectTransform)rt.parent).rect.width - minX;
+        Debug.Log(gameObject.name + ": " + minX + "/" + maxX + " AnchorP: " + rt.anchoredPosition + " LocalP: " + rt.localPosition + " WorldP: " + rt.position);
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        float toX = Mathf.Clamp(rt.anchoredPosition.x + eventData.delta.x, minX, maxX);
+        Debug.Log("Local: " + rt.anchoredPosition.x + " Delta: " + eventData.delta.x + " ToX: " + toX);
+        rt.anchoredPosition = new Vector2(toX, rt.anchoredPosition.y);
+    }
+}
