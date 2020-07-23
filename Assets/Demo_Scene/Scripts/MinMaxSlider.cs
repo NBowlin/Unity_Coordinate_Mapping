f3fec9a271f580cbeeb91f8a659fbe32fdb19deb
Nick Bowlin <ngb5601@gmail.com>
2020-07-23 12:00:13 -0400

    Adding value ranges to MinMaxSlider

--- Assets/Demo_Scene/Scripts/MinMaxSlider.cs
+++ Assets/Demo_Scene/Scripts/MinMaxSlider.cs
@@ -11,13 +11,21 @@ public class MinMaxSlider : MonoBehaviour
 
     [SerializeField] private RectTransform leftHandle;
     [SerializeField] private RectTransform rightHandle;
+    private float handleRange = 0f;
 
     [SerializeField] private float minValue = 0f;
     [SerializeField] private float maxValue = 100f;
+    private float valueRange = 100f;
+
+    public float currMinValue { get; private set; }
+    public float currMaxValue { get; private set; }
 
     private void Start() {
         minX = leftHandle.rect.width / 2;
         maxX = ((RectTransform)transform).rect.width - minX;
+
+        valueRange = maxValue - minValue;
+        handleRange = maxX - minX;
     }
 
     //These functioned are called via an Event Trigger on the handles
@@ -30,9 +38,11 @@ public class MinMaxSlider : MonoBehaviour
             if (rightHandle.anchoredPosition.x >= maxX) { return; }
 
             rightHandle.anchoredPosition = new Vector2((toX + leftHandle.rect.width / 2f) + (rightHandle.rect.width / 2f), rightHandle.anchoredPosition.y);
+            currMaxValue = calculateSliderValue(rightHandle.anchoredPosition.x);
         }
 
         leftHandle.anchoredPosition = new Vector2(toX, leftHandle.anchoredPosition.y);
+        currMinValue = calculateSliderValue(toX);
     }
 
     public void RightHandleDragged(BaseEventData data) {
@@ -44,8 +54,14 @@ public class MinMaxSlider : MonoBehaviour
             if(leftHandle.anchoredPosition.x <= minX) { return; }
 
             leftHandle.anchoredPosition = new Vector2((toX - rightHandle.rect.width / 2f) - (leftHandle.rect.width / 2f), leftHandle.anchoredPosition.y);
+            currMinValue = calculateSliderValue(leftHandle.anchoredPosition.x);
         }
 
         rightHandle.anchoredPosition = new Vector2(toX, rightHandle.anchoredPosition.y);
+        currMaxValue = calculateSliderValue(toX);
+    }
+
+    private float calculateSliderValue(float x) {
+        return minValue + ((x - minX) / handleRange) * valueRange;
     }
 }
