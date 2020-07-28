f7243718ac00819b16deae1e0c7b796bf54edccf
Nick Bowlin <ngb5601@gmail.com>
2020-07-28 12:55:52 -0400

    Airplanes now properly fly in arc between ports over planet

--- Assets/Airplanes_Demo_Scene/Scripts/Airplane.cs
+++ Assets/Airplanes_Demo_Scene/Scripts/Airplane.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CoordinateMapper;
 
 public class Airplane : MonoBehaviour
 {
@@ -11,10 +12,15 @@ public class Airplane : MonoBehaviour
     [HideInInspector] public Transform to;
     [HideInInspector] public float flightTime;
 
-    public float startTime;
+    [HideInInspector] public Transform planet;
+
+    private float startTime;
+    private float maxOffset = 0.1f;
 
     private void Start() {
         startTime = Time.time;
+        planet = GameObject.Find("Earth").transform; //TODO: Don't like this but quick and dirty
+
         Invoke("Despawn", flightTime + 1f);
     }
 
@@ -25,8 +31,17 @@ public class Airplane : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var dir = to.position - from.position;
         float fracComplete = (Time.time - startTime) / flightTime;
-        transform.position = Vector3.Lerp(from.position, to.position, fracComplete);
+        if(fracComplete > 1) {
+            transform.position = to.position;
+            return;
+        }
+        var pos = Vector3.Lerp(from.position, to.position, fracComplete);
+        var ePoint = PlanetUtility.LineFromOriginToSurface(planet, pos, LayerMask.GetMask("Planet"));
+
+        if (ePoint.HasValue) {
+            var offsetFrac = fracComplete > 0.5f ? ((1f - fracComplete) * 2f) : fracComplete * 2f;
+            transform.position = ePoint.Value.point + pos * (maxOffset * offsetFrac);
+        }
     }
 }
