89cc0c0758787e8ba3fb71f2336d11b14065291e
Nick Bowlin <ngb5601@gmail.com>
2020-07-28 13:56:32 -0400

    Adding script to quit (with escape) application in actual builds

---
+++ Assets/Earthquakes_Demo_Scene/Scripts/ApplicationQuitter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationQuitter : MonoBehaviour {
+    void Update() {
+        if (Input.GetKey("escape")) { Application.Quit(); }
+    }
+}
