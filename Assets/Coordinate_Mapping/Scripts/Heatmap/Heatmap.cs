d42e1038696510a0bd90857f92f68c9a1f7fea8f
Nick Bowlin <ngb5601@gmail.com>
2020-08-06 18:02:28 -0400

    Moving heatmap generation functions to their own class

--- Assets/Coordinate_Mapping/Scripts/Heatmap/Heatmap.cs
+++ Assets/Coordinate_Mapping/Scripts/Heatmap/Heatmap.cs
@@ -16,47 +16,16 @@ namespace CoordinateMapper {
         [SerializeField] private Gradient colors;
         [SerializeField] private Renderer hmRenderer;
 
-        //TODO: Updated the class to use ICoordinatePoint - check back after rework
-        private IEnumerable<ICoordinatePoint> points;
-
         public async void GenerateHeatmapGrid(IEnumerable<ICoordinatePoint> points) {
-            this.points = points;
-            //Invoke("TestProfile", 2.0f);
+            //var overlay = await HeatmapGenerator.GenerateHeatmapAsync(heatmapSize, mPlanetRadius, kmRange, startValue, endValue, colors, points);
+            //var overlay = HeatmapGenerator.GenerateHeatmap(heatmapSize, mPlanetRadius, kmRange, startValue, endValue, colors, points);
 
-            int[,] heatmapGrid;
-            heatmapGrid = await Task.Run(() => Heatmap.GenerateValues((int)heatmapSize.x, (int)heatmapSize.y, mPlanetRadius, kmRange, startValue, endValue, colors, points));
-
-            DrawHeatmapGrid(heatmapGrid);
-
-            var colorBytes = await Task.Run(() => Texture2D_Extensions.CreateColorMap(heatmapGrid, colors));
-
-            Texture2D overlay = new Texture2D(heatmapGrid.GetLength(0), heatmapGrid.GetLength(1), TextureFormat.RGBA32, false);
-            overlay.LoadRawTextureData(colorBytes);
-            overlay.Apply();
-
+            var grid = await Task.Run(() => HeatmapGenerator.GenerateValues((int)heatmapSize.x, (int)heatmapSize.y, mPlanetRadius, kmRange, startValue, endValue, points));
+            var colorBytes = await Task.Run(() => HeatmapGenerator.CreateColorMap(grid, colors));
+            var overlay = HeatmapGenerator.CreateHeatmapTexture((int)heatmapSize.x, (int)heatmapSize.y, colorBytes);
             hmRenderer.material.SetTexture("_OverlayTex", overlay);
 
-            /*int[,] heatmapGrid;
-
-            var sw = System.Diagnostics.Stopwatch.StartNew();
-
-            UnityEngine.Profiling.Profiler.BeginSample("HeatmapGenerate");
-            heatmapGrid = await Heatmap.GenerateValues((int)heatmapSize.x, (int)heatmapSize.y, mPlanetRadius, kmRange, startValue, endValue, colors, points);
-            UnityEngine.Profiling.Profiler.EndSample();
-
-            sw.Stop();
-            Debug.Log("Heatmap generation time: " + sw.ElapsedMilliseconds / 1000f);
-            //sw.Reset();
-
-            DrawHeatmapGrid(heatmapGrid);
-
-            //sw.Start();
-            UnityEngine.Profiling.Profiler.BeginSample("HeatmapDraw");
-            Texture2D overlay = Texture2D_Extensions.DrawHeatmap(heatmapGrid, colors);
-            hmRenderer.material.SetTexture("_OverlayTex", overlay);
-            UnityEngine.Profiling.Profiler.EndSample();
-            //sw.Stop();
-            //Debug.Log("Heatmap draw time: " + sw.ElapsedMilliseconds / 1000f);*/
+            DrawHeatmapGrid(grid);
         }
 
         void DrawHeatmapGrid(int[,] heatmapGrid) {
@@ -85,174 +54,6 @@ namespace CoordinateMapper {
 
             Debug.DrawLine(new Vector3(right, transform.position.y, bot), new Vector3(right, transform.position.y, top), Color.green, 100f);
             Debug.DrawLine(new Vector3(left, transform.position.y, top), new Vector3(right, transform.position.y, top), Color.green, 100f);
-        }
-
-        //TODO: Make this more efficient
-        public static int[,] GenerateValues(int w, int h, float radius, float range, int startValue, int endValue, Gradient colors, IEnumerable<ICoordinatePoint> points) {
-            int[,] heatmapGrid = new int[w, h];
-
-            //TODO: Check vs lat/lng of center grid space for x/y (currently bot left of grid space)
-            //Or maybe even find min and max lat/lng and check if distance threshold is inside that?
-
-            //float eC = 40075.017f;
-            //float kmPerLng = eC / 360f;
-            //float degPerLng = 360f / w;
-            //float cellWidthKm = kmPerLng * degPerLng;
-            //int widthCells = Mathf.RoundToInt(range / cellWidthKm);
-
-            //Latitude lines are constant distance - so pre-calculate the km between latitudes and use that to restrict
-            //the y coords as we check location distance below
-            float kmPerLat = Mathf.PI * radius / 180.0f / 1000f; //km per 1 degree of latitude (Earth is ~111km)
-            float degPerLat = 180f / h;
-            float cellHeightKm = kmPerLat * degPerLat;
-            int cellRangeY = Mathf.RoundToInt(range / cellHeightKm);
-
-            //var sw = new System.Diagnostics.Stopwatch();
-
-            foreach (ICoordinatePoint p in points) {
-                float texLat = 90f + p.location.latitude;
-                float texLng = 180f + p.location.longitude;
-
-                float latRatio = texLat / 180f;
-                float lngRatio = texLng / 360f;
-
-                float xCenter = Mathf.Round(lngRatio * w);
-                float yCenter = Mathf.Round(latRatio * h);
-
-                xCenter = (int)Mathf.Clamp(xCenter, 0f, w - 1);
-                yCenter = (int)Mathf.Clamp(yCenter, 0f, h - 1);
-
-                //Calculate longitudinal range for given point: 1° Longitude = Cos(lat) * length of 1° (km) at equator
-                float yLat = (float)yCenter / (float)h * 180f - 90f;
-                float kmPerLng = Mathf.Cos(yLat * Mathf.Deg2Rad) * kmPerLat;
-                float degPerLng = 360f / w;
-                float cellWidthKm = kmPerLng * degPerLng;
-                int cellRangeX = Mathf.RoundToInt(range / cellWidthKm);
-
-                //The idea was to only do distance calculations for 1/4th of the circle, then just mirror those values around the grid
-                //However, because bot-left lat/lng of circle is farther from center than, say, bot-right, it doesn't quite work
-                //Could adjust where I'm checking lat/lng, but moving on for now
-                /*heatmapGrid[(int)xCenter, (int)yCenter] += startValue;
-                int[,] currValues = new int[cellRangeX + 1, cellRangeY + 1];
-                //Bottom left quadrant
-                for (int x = (int)xCenter - cellRangeX; x <= xCenter; x++) {
-                    var currX = x;
-                    if (x < 0 || x >= w) {
-                        if (cellRangeX < w / 2) { currX = Modulo(x, w); }
-                        else { continue; }
-                    }
-
-                    for (int y = (int)yCenter - cellRangeY; y <= yCenter; y++) {
-                        if (y < 0 || y >= h || (currX == (int)xCenter && y == (int)yCenter)) { continue; }
-
-                        float lng = CartesianToSphericalLongitude((float)currX, (float)w);
-                        float lat = CartesianToSphericalLatitude((float)y, (float)h);
-                        float d = p.location.kmBetweenLocations(lat, lng);
-
-                        if (d < range) {
-                            float dRatio = d / (float)range;
-                            int fallOffRange = startValue - endValue;
-                            int fallOffVal = (int)(startValue - (fallOffRange * dRatio));
-                            heatmapGrid[currX, y] += fallOffVal;
-
-                            currValues[(int)xCenter - x, (int)yCenter - y] = fallOffVal;
-                        }
-                    }
-                }
-
-                //Bottom right quadrant
-                for (int x = (int)xCenter + 1; x <= xCenter + cellRangeX; x++) {
-                    var currX = x;
-                    if (x < 0 || x >= w) {
-                        if (cellRangeX < w / 2) { currX = Modulo(x, w); }
-                        else { continue; }
-                    }
-
-                    for (int y = (int)yCenter - cellRangeY; y < yCenter; y++) {
-                        if (y < 0 || y >= h || (currX == (int)xCenter && y == (int)yCenter)) { continue; }
-                        heatmapGrid[currX, y] += currValues[x - (int)xCenter, (int)yCenter - y];
-                    }
-                }
-
-                //Top left quadrant
-                for (int x = (int)xCenter - cellRangeX; x < xCenter; x++) {
-                    var currX = x;
-                    if (x < 0 || x >= w) {
-                        if (cellRangeX < w / 2) { currX = Modulo(x, w); }
-                        else { continue; }
-                    }
-
-                    for (int y = (int)yCenter + 1; y <= yCenter + cellRangeY; y++) {
-                        if (y < 0 || y >= h || (currX == (int)xCenter && y == (int)yCenter)) { continue; }
-                    }
-                }
-
-                //Top right quadrant
-                for (int x = (int)xCenter; x <= xCenter + cellRangeX; x++) {
-                    var currX = x;
-                    if (x < 0 || x >= w) {
-                        if (cellRangeX < w / 2) { currX = Modulo(x, w); }
-                        else { continue; }
-                    }
-
-                    for (int y = (int)yCenter; y <= yCenter + cellRangeY; y++) {
-                        if (y < 0 || y >= h || (currX == (int)xCenter && y == (int)yCenter)) { continue; }
-                    }
-                }*/
-
-
-
-                for (int x = (int)xCenter - cellRangeX; x <= xCenter + cellRangeX; x++) {
-                    var currX = x;
-                    if (x < 0 || x >= w) {
-                        if (cellRangeX < w / 2) { currX = Modulo(x, w); }
-                        else { continue; }
-                    }
-
-                    for (int y = (int)yCenter - cellRangeY; y <= yCenter + cellRangeY; y++) {
-                        if (y < 0 || y >= h) { continue; }
-
-                        float lng = CartesianToSphericalLongitude((float)currX, (float)w);
-                        float lat = CartesianToSphericalLatitude((float)y, (float)h);
-                        //sw.Start();
-                        float d = p.location.kmBetweenLocations(lat, lng, radius);
-                        //sw.Stop();
-
-                        if (d < range) {
-                            float dRatio = d / range;
-                            int fallOffRange = startValue - endValue;
-                            int fallOffVal = (int)(startValue - (fallOffRange * dRatio));
-                            heatmapGrid[currX, y] += fallOffVal;
-                        }
-                    }
-                }
-            }
-
-            //Debug.Log("Heatmap trig time: " + sw.ElapsedMilliseconds / 1000f + " | ACos time: " + Location.sw.ElapsedMilliseconds / 1000f + " | Sin Lookup: " + LookupTable.sinSw.ElapsedMilliseconds / 1000f + " | Cos Lookup: " + LookupTable.cosSw.ElapsedMilliseconds / 1000f
-            //     + " | Abs time: " + LookupTable.absSw.ElapsedMilliseconds / 1000f);
-
-            return heatmapGrid;
-        }
-
-        public static (float latitude, float longitude) CartesionToSphericalCoords(float x, float y, float gridWidth, float gridHeight) {
-            float lng = CartesianToSphericalLongitude(x, gridWidth);
-            float lat = CartesianToSphericalLatitude(y, gridHeight);
-            return (lat, lng);
-        }
-
-        public static float CartesianToSphericalLatitude(float y, float gridHeight) {
-            //var centerY = y + (y >= gridHeight / 2  ? 0.5f :-0.5f); //Test vs the center of the grid point, rather than the origin
-            return y / gridHeight * 180f - 90f;
-        }
-
-        public static float CartesianToSphericalLongitude(float x, float gridWidth) {
-            //var centerX = x + (x >= gridWidth / 2 ? 0.5f : -0.5f); //Test vs the center of the grid point, rather than the origin
-            return x / gridWidth * 360f - 180f;
-        }
-
-        //Apparently C# % operator is actually just remainder. So negative numbers don't mod properly, below is proper modulo
-        public static int Modulo(int a, int b) {
-            return a - b * Mathf.FloorToInt((float)a / (float)b);
         }
     }
 }
---
+++ Assets/Coordinate_Mapping/Scripts/Heatmap/HeatmapGenerator.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CoordinateMapper;
+using System.Threading.Tasks;
+
+namespace CoordinateMapper {
+    public static class HeatmapGenerator {
+
+        public static Texture2D GenerateHeatmap(Vector2 heatmapSize, float mPlanetRadius, float kmRange, int startValue, int endValue, Gradient colors, IEnumerable<ICoordinatePoint> points) {
+            var heatmapGrid = GenerateValues((int)heatmapSize.x, (int)heatmapSize.y, mPlanetRadius, kmRange, startValue, endValue, points);
+            var colorBytes = CreateColorMap(heatmapGrid, colors);
+
+            return CreateHeatmapTexture(heatmapGrid.GetLength(0), heatmapGrid.GetLength(1), colorBytes);
+        }
+
+        public async static Task<Texture2D> GenerateHeatmapAsync(Vector2 heatmapSize, float mPlanetRadius, float kmRange, int startValue, int endValue, Gradient colors, IEnumerable<ICoordinatePoint> points) {
+            var heatmapGrid = await Task.Run(() => GenerateValues((int)heatmapSize.x, (int)heatmapSize.y, mPlanetRadius, kmRange, startValue, endValue, points));
+            var colorBytes = await Task.Run(() => CreateColorMap(heatmapGrid, colors));
+
+            return CreateHeatmapTexture(heatmapGrid.GetLength(0), heatmapGrid.GetLength(1), colorBytes);
+        }
+
+        //NOTE: Texture operations must be done on the main thread - so don't call this asynchronously
+        public static Texture2D CreateHeatmapTexture(int width, int height, byte[] colorBytes) {
+            Texture2D heatmap = new Texture2D(width, height, TextureFormat.RGBA32, false);
+            heatmap.LoadRawTextureData(colorBytes);
+            heatmap.Apply();
+            return heatmap;
+        }
+
+        //TODO: Make this more efficient
+        public static int[,] GenerateValues(int w, int h, float radius, float range, int startValue, int endValue, IEnumerable<ICoordinatePoint> points) {
+            int[,] heatmapGrid = new int[w, h];
+
+            //Latitude lines are constant distance - so pre-calculate the km between latitudes and use that to restrict
+            //the y coords as we check location distance below
+            float kmPerLat = Mathf.PI * radius / 180.0f / 1000f; //km per 1 degree of latitude (Earth is ~111km)
+            float degPerLat = 180f / h;
+            float cellHeightKm = kmPerLat * degPerLat;
+            int cellRangeY = Mathf.RoundToInt(range / cellHeightKm);
+
+            foreach (ICoordinatePoint p in points) {
+                float texLat = 90f + p.location.latitude;
+                float texLng = 180f + p.location.longitude;
+
+                float latRatio = texLat / 180f;
+                float lngRatio = texLng / 360f;
+
+                float xCenter = Mathf.Round(lngRatio * w);
+                float yCenter = Mathf.Round(latRatio * h);
+
+                xCenter = (int)Mathf.Clamp(xCenter, 0f, w - 1);
+                yCenter = (int)Mathf.Clamp(yCenter, 0f, h - 1);
+
+                //Calculate longitudinal range for given point: 1° Longitude = Cos(lat) * length of 1° (km) at equator
+                float yLat = (float)yCenter / (float)h * 180f - 90f;
+                float kmPerLng = Mathf.Cos(yLat * Mathf.Deg2Rad) * kmPerLat;
+                float degPerLng = 360f / w;
+                float cellWidthKm = kmPerLng * degPerLng;
+                int cellRangeX = Mathf.RoundToInt(range / cellWidthKm);
+
+                for (int x = (int)xCenter - cellRangeX; x <= xCenter + cellRangeX; x++) {
+                    var currX = x;
+                    if (x < 0 || x >= w) {
+                        if (cellRangeX < w / 2) { currX = Modulo(x, w); }
+                        else { continue; }
+                    }
+
+                    for (int y = (int)yCenter - cellRangeY; y <= yCenter + cellRangeY; y++) {
+                        if (y < 0 || y >= h) { continue; }
+
+                        float lng = CartesianToSphericalLongitude((float)currX, (float)w);
+                        float lat = CartesianToSphericalLatitude((float)y, (float)h);
+                        float d = p.location.kmBetweenLocations(lat, lng, radius);
+
+                        if (d < range) {
+                            float dRatio = d / range;
+                            int fallOffRange = startValue - endValue;
+                            int fallOffVal = (int)(startValue - (fallOffRange * dRatio));
+                            heatmapGrid[currX, y] += fallOffVal;
+                        }
+                    }
+                }
+            }
+
+            return heatmapGrid;
+        }
+
+        public static (float latitude, float longitude) CartesionToSphericalCoords(float x, float y, float gridWidth, float gridHeight) {
+            float lng = CartesianToSphericalLongitude(x, gridWidth);
+            float lat = CartesianToSphericalLatitude(y, gridHeight);
+            return (lat, lng);
+        }
+
+        public static float CartesianToSphericalLatitude(float y, float gridHeight) {
+            //var centerY = y + (y >= gridHeight / 2  ? 0.5f :-0.5f); //Test vs the center of the grid point, rather than the origin
+            return y / gridHeight * 180f - 90f;
+        }
+
+        public static float CartesianToSphericalLongitude(float x, float gridWidth) {
+            //var centerX = x + (x >= gridWidth / 2 ? 0.5f : -0.5f); //Test vs the center of the grid point, rather than the origin
+            return x / gridWidth * 360f - 180f;
+        }
+
+        //Apparently C# % operator is actually just remainder. So negative numbers don't mod properly, below is proper modulo
+        public static int Modulo(int a, int b) {
+            return a - b * Mathf.FloorToInt((float)a / (float)b);
+        }
+
+        public static byte[] CreateColorMap(int[,] heatmapValues, Gradient colors) {
+            int w = heatmapValues.GetLength(0);
+            int h = heatmapValues.GetLength(1);
+
+            var texColors = new Color32[w * h];
+            for (int i = 0; i < texColors.Length; i++) { texColors[i] = Color.clear; }
+
+            for (int y = 0; y < h; y++) {
+                for (int x = 0; x < w; x++) {
+                    if (heatmapValues[x, y] > 0) {
+                        texColors[y * w + x] = colors.Evaluate(heatmapValues[x, y] / 100f);
+                    }
+                }
+            }
+
+            return ColorsToBytes(texColors);
+        }
+
+        public static byte[] ColorsToBytes(Color32[] colors) {
+            var byteColors = new byte[colors.Length * 4];
+            for (int i = 0; i < colors.Length; i++) {
+                Color32 c = colors[i];
+                byteColors[i * 4] = c.r;
+                byteColors[i * 4 + 1] = c.g;
+                byteColors[i * 4 + 2] = c.b;
+                byteColors[i * 4 + 3] = c.a;
+            }
+            return byteColors;
+        }
+    }
+}
